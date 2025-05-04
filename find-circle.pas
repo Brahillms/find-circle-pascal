3ad3d481f50abf88855922fb36956091b138a404
Raspberreeee <57116637+Brahillms@users.noreply.github.com>
2025-05-04 12:23:15 -0400

    Hopefully no more runtime errors

--- find-circle.pas
+++ find-circle.pas
@@ -2,16 +2,21 @@ program findCircle;
 
 {$mode objfpc}{$H+}
 
-uses Math;
+uses Math, sysutils;
 
 var
 	mode: char;
-	radius, circumference, area: single;
+	radius, circumference: string;
+	convRadius, convCircumference, area: single;
+	loopShouldBreak: boolean;
 
 const
 	tau = pi * 2;
+	failedTryStrToFloat = 'You must enter a numerical value, try again.';
 
 begin
+	loopShouldBreak := false;
+
 	writeLn('Please select an option listed below from 1 to 4.');
 	writeLn('[1]'+ #9 + 'Find circumference from radius' + sLineBreak + '[2]' + #9 + 'Find radius from circumference' + sLineBreak + '[3]' + #9 + 'Find diameter from radius' + sLineBreak + '[4]' + #9 + 'Find circle area from radius');
 	writeLn('Enter any other key to exit.');
@@ -21,44 +26,79 @@ begin
 	case (mode) of
 		'1' :
 			{Circumference mode}
+			while loopShouldBreak = false do
 			begin
 				write(sLineBreak + 'Please put the radius of your circle you want to find the circumference of: ');
 				readLn(radius);
-			
-				circumference := radius * tau;
+				
+				if TryStrToFloat(radius, convRadius) = true then
+				begin
+					ConvCircumference := convRadius * tau;
 
-				writeLn('Your circle''s circumference is ', circumference:1:6, '.');
+					writeLn('Your circle''s circumference is ', convCircumference:1:6, '.');
+					loopShouldBreak := true;
+				end
+				else
+				begin
+					writeLn(failedTryStrToFloat);
+				end;
 			end;
 		'2' :
 			{Radius mode}
+			while loopShouldBreak = false do
 			begin
 				write(sLineBreak + 'Please put the circumference of your circle you want to find the radius of: ');
 				readLn(circumference);
 				
-				radius := circumference / tau;
+				if TryStrToFloat(circumference, convCircumference) = true then
+				begin
+					ConvRadius := ConvCircumference / tau;
 
-				writeLn('Your circle''s radius is ', radius:1:6, '.');
+					writeLn('Your circle''s radius is ', ConvRadius:1:6, '.');
+					loopShouldBreak := true;
+				end
+				else
+				begin
+					writeLn(failedTryStrToFloat);
+				end;
 			end;
 		'3' :
 			{Diameter mode}
+			while loopShouldBreak = false do
 			begin
 				write(sLineBreak + 'Please put the radius of your circle you want to find the diameter of: ');
 				readLn(radius);
+				
+				if TryStrToFloat(radius, convRadius) = true then
+				begin
+					convRadius := convRadius * 2;
 
-				radius := radius * 2;
-
-				writeLn('Your circle''s diameter is ', radius:1:6, '.');
+					writeLn('Your circle''s diameter is ', convRadius:1:6, '.');
+					loopShouldBreak := true;
+				end
+				else
+				begin
+					writeLn(failedTryStrToFloat);
+				end;
 			end;
 		'4' :
 			{Area mode}
+			while loopShouldBreak = false do
 			begin
 				write(sLineBreak + 'Please put the radius of your circle you want to find the area of: ');
 				readLn(radius);
+				
+				if TryStrToFloat(radius, convRadius) = true then
+				begin
+					area := pi * Power(convRadius, 2);
 
-				area := pi * Power(radius, 2);
-
-				writeLn('Your circle''s area is ', area:1:6, '.');
+					writeLn('Your circle''s area is ', area:1:6, '.');
+					loopShouldBreak := true;
+				end
+				else
+				begin
+					writeLn(failedTryStrToFloat);
+				end;
 			end;
 	end;
 end.
-
